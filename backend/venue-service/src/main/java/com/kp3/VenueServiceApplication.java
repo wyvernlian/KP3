8a9f20e8f3cbb6fa81778a53afbf73c3e57c4761
wyvernlian <6527547@qq.com>
2026-03-03 15:39:02 +0800

    修改

--- backend/venue-service/src/main/java/com/kp3/VenueServiceApplication.java
+++ backend/venue-service/src/main/java/com/kp3/VenueServiceApplication.java
@@ -2,10 +2,8 @@ package com.kp3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class VenueServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(VenueServiceApplication.class, args);
